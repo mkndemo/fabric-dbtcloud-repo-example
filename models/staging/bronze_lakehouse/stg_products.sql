8b58fec6f39a4559e767844f3755049abf015f7e
Max Knutsson <max.knutsson+demo@dbtlabs.com>
2024-09-20 15:02:40 +0000

    fabric project based on jaffle shop

--- models/staging/bronze_lakehouse/stg_products.sql
+++ models/staging/bronze_lakehouse/stg_products.sql
@@ -9,24 +9,15 @@ source as (
 renamed as (
 
     select
-
-        ----------  ids
         sku as product_id,
-
-        ---------- text
         name as product_name,
         type as product_type,
         description as product_description,
-
-
-        ---------- numerics
+        
+        case when type = 'jaffle' then 1 else 0 end as is_food_item,
+        case when type = 'beverage' then 1 else 0 end as is_drink_item,
+        
         {{ cents_to_dollars('price') }} as product_price
-
-        ---------- booleans
-       -- coalesce(type = 'jaffle', false) as is_food_item,
-
-       -- coalesce(type = 'beverage', false) as is_drink_item
-
     from source
 
 )
